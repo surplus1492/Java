67298a7c08f3bd13140231c3f8636f458c59f688
surplus1492 <dudgus960408@naver.com>
2021-05-17 11:36:15 +0900

    2021/05/17 Java Ch01

---
+++ Ch01/src/sub1/HelloWorld.java
@@ -0,0 +1,15 @@
+package sub1;
+
+/*
+ * 2021/05/17
+ * 김영현
+ * HelloWorld 출력
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	}
+}
